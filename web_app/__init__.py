4603775c6506d57a8b68210f2c22d74bf33dac9d
Amy Beisel <amy.beisel@gmail.com>
2020-04-28 10:02:03 -0600

    iris route example

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -8,6 +8,8 @@ from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
+from web_app.routes.stats_routes import stats_routes
+
 
 DATABASE_URI = "sqlite:////Users/amybeisel/Code_Here/twitoff-amy/twitoff_development.db"
 SECRET_KEY = "super secret"
@@ -24,6 +26,8 @@ def create_app():
     app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
     app.register_blueprint(admin_routes)
+    app.register_blueprint(stats_routes)
+
 
     return app
 
